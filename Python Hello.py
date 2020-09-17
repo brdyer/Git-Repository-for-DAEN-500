b324655c256656273ef146dbdc3a3ed7d22857f5
doug-cady <70990621+doug-cady@users.noreply.github.com>
2020-09-16 21:29:48 -0500

    Added other examples for printing greetings
    Hey Brad,
    I added a couple other examples for your pleasure.
    ~ Doug

--- Python Hello.py
+++ Python Hello.py
@@ -1,1 +1,39 @@
-print('Hello Professor Berlin')
+# Brad's greeting:
+# print('Hello Professor Berlin')
+
+
+# -------------------------------------------
+# Other ways to print a greeting message:
+# 1)
+greeting = ['Hello', 'Professor', 'Berlin']
+
+# We can use .join(<a list of strs>) to print our greeting
+print(' '.join(greeting))
+# > Hello Professor Berlin
+
+# or
+print('\n'.join(greeting))
+""" out:
+Hello
+Professor
+Berlin
+"""
+
+
+# 2) Using a for loop over the items (key, val)
+# from a dictionary combined with string
+# justification (left, center)
+# Also, f strings - they do the same as '{}'.format()
+greet_dict = {
+    'first':  'Hello',
+    'second': 'Professor',
+    'third':  'Berlin'
+}
+
+print('\n'.join(f"| {key:>8} | {val:^15} |"
+                for key, val in greet_dict.items()))
+""" out:
+|    first |      Hello      |
+|   second |    Professor    |
+|    third |     Berlin      |
+"""
